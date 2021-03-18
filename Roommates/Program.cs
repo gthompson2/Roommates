e8ecc08497bd975c3d5c23f6747985c111758c7b
Gregory Thompson <mrgregorythompsonii@gmail.com>
2021-03-18 16:20:27 -0500

    initial commit

---
+++ Roommates/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using Roommates.Repositories;
+using Roommates.Models;
+using Microsoft.Data.SqlClient;
+
+
+namespace Roommates
+{
+    class Program
+    {
+        //  This is the address of the database.
+        //  We define it here as a constant since it will never change.
+        private const string CONNECTION_STRING = @"server=localhost\SQLExpress;database=Roommates;integrated security=true";
+
+        static void Main(string[] args)
+        {
+            RoomRepository roomRepo = new RoomRepository(CONNECTION_STRING);
+
+            bool runProgram = true;
+            while (runProgram)
+            {
+                string selection = GetMenuSelection();
+
+                switch (selection) // Based on the selection string, a command will be executed
+                {
+                    case ("Show all rooms"):
+                        List<Room> rooms = roomRepo.GetAll();
+                        foreach (Room r in rooms)
+                        {
+                            Console.WriteLine($"{r.Name} has an Id of {r.Id} and a max occupancy of {r.MaxOccupancy}");
+                        }
+                        Console.Write("Press any key to continue");
+                        Console.ReadKey();
+                        break;
+                    case ("Search for room"):
+                        // Do stuff
+                        break;
+                    case ("Add a room"):
+                        // Do stuff
+                        break;
+                    case ("Exit"):
+                        runProgram = false;
+                        break;
+                }
+            }
+
+        }
+
+        static string GetMenuSelection() // Returns a string representing the command that should be executed by the program
+        {
+            Console.Clear();
+
+            List<string> options = new List<string>()
+            {
+                "Show all rooms",
+                "Search for room",
+                "Add a room",
+                "Exit"
+            };
+
+            for (int i = 0; i < options.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {options[i]}");
+            }
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine();
+                    Console.Write("Select an option > ");
+
+                    string input = Console.ReadLine();
+                    int index = int.Parse(input) - 1;
+                    return options[index]; // returns the string in options corresponding to the number chosen
+                }
+                catch (Exception)
+                {
+
+                    continue; // If an invalid index is typed by the user, the while loop reexecutes 
+                }
+            }
+        }
+    }
+}
